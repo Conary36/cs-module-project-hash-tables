69cd430270aaed6bc74fdb03cc972c369f2b1077
Conary36 <cbeckford144@gmail.com>
2020-11-04 12:41:54 -0500

    ...sample run caesar...

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,29 @@
 
 # Your code here
 
+message = 'HRIXAS ZJGKM YDLUB WPCNQ OTEFV'
+ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(ABC)):
+
+    translated = ''
+
+    for symbol in message:
+        if symbol in ABC:
+            num = ABC.find(symbol)
+            num = num - key
+
+        if num < 0:
+            num = num + len(ABC)
+
+        translated = translated + ABC[num]
+
+    else:
+        translated = translated + symbol
+    print('Key #%s: %s' % (key, translated))
+
+
+
+
+
+
