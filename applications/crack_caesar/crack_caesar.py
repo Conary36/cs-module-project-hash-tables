2a9f3ec2e4e85ed164350436ef32f0200199f305
Conary36 <cbeckford144@gmail.com>
2020-11-05 20:15:24 -0500

    ...saving...

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,30 +2,95 @@
 # decode it.
 
 # Your code here
-
-message = 'HRIXAS ZJGKM YDLUB WPCNQ OTEFV'
-ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-for key in range(len(ABC)):
-
-    translated = ''
-
-    for symbol in message:
-        if symbol in ABC:
-            num = ABC.find(symbol)
-            num = num - key
-
-        if num < 0:
-            num = num + len(ABC)
-
-        translated = translated + ABC[num]
-
-    else:
-        translated = translated + symbol
-    print('Key #%s: %s' % (key, translated))
+# def load_names(path):
+#     with open(path) as text_file:
+#         return text_file.read().splitlines()
+#
+# names = load_names('names.txt')
+def load_cipher(path):
+    with open('ciphertext.txt', 'r') as reader:
+        print(reader.read())
+        return reader.read().splitlines()
 
 
+encode_table = {
+    'A': 'H',
+    'B': 'Z',
+    'C': 'Y',
+    'D': 'W',
+    'E': 'O',
+    'F': 'R',
+    'G': 'J',
+    'H': 'D',
+    'I': 'P',
+    'J': 'T',
+    'K': 'I',
+    'L': 'G',
+    'M': 'L',
+    'N': 'C',
+    'O': 'E',
+    'P': 'X',
+    'Q': 'K',
+    'R': 'U',
+    'S': 'N',
+    'T': 'F',
+    'U': 'A',
+    'V': 'M',
+    'W': 'B',
+    'X': 'Q',
+    'Y': 'V',
+    'Z': 'S'
+}
+
+first_string = 'hello'
+words = load_cipher('ciphertext.txt')
+
+def encode(old_string):
+    new_string = ''
+
+    for symbol in old_string.upper():
+        new_string = new_string + encode_table[symbol]
+
+        return new_string
 
 
+raggle = encode(words)
+print(raggle)
+
+decode_table = {}
+for key, value in encode_table.items():
+    decode_table[value] = key
 
 
+def decode(old_string):
+    new_string = ''
+
+    for letter in old_string.upper():
+        new_string = new_string + decode_table[letter]
+
+    return new_string
+
+
+decoded = decode(raggle)
+print(decoded)
+
+# message = 'HRIXAS ZJGKM YDLUB WPCNQ OTEFV'
+# ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#
+# for key in range(len(ABC)):
+#
+#     translated = ''
+#
+#     for symbol in message:
+#         if symbol in ABC:
+#             num = ABC.find(symbol)
+#             num = num - key
+#
+#         if num < 0:
+#             num = num + len(ABC)
+#
+#         translated = translated + ABC[num]
+#
+#     else:
+#         translated = translated + symbol
+#     print('Key #%s: %s' % (key, translated))
--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -3,8 +3,14 @@
 
 def expensive_seq(x, y, z):
     # Your code here
+   expens(x,y,z) = 0
 
+    if x <= 0:
+        y + z
+    elif x > 0:
+        expens(x - 1, y + 1, z) + expens(x - 2, y + 2, z * 2) + expens(x - 3, y + 3, z * 3)
 
+    return expensive_seq(x,y,z)
 
 if __name__ == "__main__":
     for i in range(10):
